62ce72ec1156f6352cb70d696e2ecb65f98294fa
JuliaBaroncello <juliaestephani.ribeiro@gmail.com>
2023-05-14 23:33:42 -0300

    parece certo

--- anot.c
+++ anot.c
@@ -16,6 +16,7 @@ int inserir_soldado(No **L, char soldado[]){
         return 0;
     }
     strcpy(novo->soldado, soldado);
+    //*novo->soldado = soldado;
     novo->prox = NULL;
     novo->ant = NULL;
     novo->id = 0;
@@ -26,9 +27,12 @@ int inserir_soldado(No **L, char soldado[]){
         novo->prox = novo;
         novo->ant = novo;
     } else{
+        //novo->id = 1;
+        //(*L)->id = novo->id+1;
         novo->prox = *L;
         novo->ant = (*L)->ant;
         novo->ant->prox = novo;
+        //(*L)->ant = novo;
         (*L)->ant = novo;
         novo->id = novo->ant->id+1;
     }
@@ -47,80 +51,115 @@ void mostrar_soldados(No **L){
     }
 }
 
-No * sortear_soldado(No **L){
+int sortear_soldado(No **L){
     srand(time(NULL));
     int sorteio = rand()%((*L)->ant->id) + 1;
-    printf("Numero do soldado: %i\n", sorteio);
+    printf("%i\n", sorteio);
     No *p;
     if (sorteio == (*L)->ant->id){
-        //printf("%s foi o soldado selecionado.\n", (*L)->ant->soldado);
-        return (*L)->ant;
+        printf("%s foi o soldado selecionado.\n", (*L)->ant->soldado);
+        return (*L)->ant->id;
     }
     for (p = *L; p->prox != *L; p = p->prox){
         if (sorteio == p->id){
-            //printf("%s foi o soldado selecionado.\n", p->soldado);
-            return p;
+            printf("%s foi o soldado selecionado.\n", p->soldado);
+            return p->id;
         }
     }
-}
-
-int remover_soldado(No **selecionado){
-    No *p;
-    p = *selecionado;
-    p->ant->prox = p->prox;
-    p->prox->ant = p->ant;
-    printf("%s foi o soldado eliminado\n", p->soldado);
-    free(p);
-    return 1;
-}
-
-No * selecionar_soldado(No **inicial, int n){
-    if (*inicial == NULL){
-        return NULL;
-    }
-    if (*inicial == (*inicial)->prox){
-        return *inicial;
-    }
-    No *aux, *proximo;
-    aux = *inicial;
-    while(aux != aux->prox){
-        for (int i = 1; i < n; i++){
-            aux = aux->prox;
-        }
-        proximo = aux->prox;
-        remover_soldado(&aux);
-        aux = proximo;
-    }
-    return aux;
 }
 
 int sortear_n(No **L){
     srand(time(NULL));
-    return (rand()%100) + 1;
+    return (rand()%11) + 1;
+    //printf("%i\n", rand()%101);
 }
+
+No * remover_soldado(No **L, int pos){
+    if (*L == NULL){
+        return NULL;
+    }
+    if (*L == (*L)->prox){
+        return *L;
+    }
+    No *p, *q;
+    int cont = 1;
+    for (p = *L; p->prox != *L; p = p->prox){
+        if (cont == pos){
+            q = p->ant;
+            q->prox = p->prox;
+            p->prox->ant = q;
+            return p;
+            free(p);
+        }
+        cont++;
+    }
+}
+/*
+No * remover_soldado(No **L, int n){
+    if (*L == NULL){
+        return NULL;
+    }
+    No *p;
+    int pos = 1;
+    No *aux;
+    int sold = sortear_soldado(&L);
+    for (aux = *L; aux->prox != *L; aux = aux->prox){
+        if (aux->id == sold){
+            *L = aux;
+        }
+    }
+    for (p = aux; p->prox != aux; p = p->prox){
+        if (pos == n){
+
+        }
+        pos++;
+    }
+    p = (*L)->ant;
+    p->ant->prox = *L;
+    (*L)->ant = p->ant;
+    free(p);
+    return 1;
+    /*
+    No *p;
+    p = *L;
+    while(p->prox != *L){
+        if (n == p->id){
+            No *q;
+            q = p;
+
+        }
+        p = p->prox;
+    }
+    */
+//}
+
 
 int menu(){
     int opc;
-    //system("clear");
+    system("clear");
     printf("[0] - Sair\n");
     printf("[1] - Inserir soldado\n");
     printf("[2] - Mostrar soldados\n");
     printf("[3] - Sortear o soldado que iniciará a contagem\n");
-    printf("[4] - Apresentar o soldado selecionado (apresente também o número n sorteado e os soldados eliminados)\n\n");
+    printf("[4] - Sortear n\n");
+    printf("[5] - Apresentar o soldado selecionado (apresente também o número n sorteado e os soldados eliminados)\n\n");
 
     printf("Escolha uma opção: ");
     scanf("%i", &opc);
     return opc;
 }
 
+/*
+[ 3 ] - Sortear o soldado que iniciará a contagem
+[ 4 ] - Apresentar o soldado selecionado (apresente também o número n sorteado e os
+soldados eliminados)
+*/
+
 int main(){
     No *L = NULL;
-    No *sorteado = NULL;
-    No *selecionado = NULL;
     char nome[50];
     int opc;
     int tam = 0;
-    int n;
 
     while(opc = menu()){
         switch (opc){
@@ -141,15 +180,14 @@ int main(){
                 break;
             
             case 3:
-                sorteado = sortear_soldado(&L);
-                printf("Soldado sorteado: %s\n", sorteado->soldado);
+                sortear_soldado(&L);
                 break;
 
             case 4:
-                n = sortear_n(&L);
-                printf("Numero sorteado: %i\n", n);
-                selecionado = selecionar_soldado(&sorteado, n);
-                printf("Soldado selecionado: id: %i, nome: %s", selecionado->id, selecionado->soldado);
+                //int n = sortear_n(&L);
+                if (remover_soldado(&L, 1)){
+                    printf("Soldado removido.\n");
+                }
                 break;
 
             default:
@@ -158,4 +196,21 @@ int main(){
         getchar();
         getchar();
     }
+
+/*
+    inserir_soldado(&L, nome);
+
+    printf("Insira seu nome: ");
+    fgets(nome, 49, stdin);
+    nome[strlen(nome)-1]='\0';
+
+    inserir_soldado(&L, nome);
+
+    printf("Insira seu nome: ");
+    fgets(nome, 49, stdin);
+    nome[strlen(nome)-1]='\0';
+
+    inserir_soldado(&L, nome);
+
+    mostrar_soldados(&L);*/
 }
